417f9a4b1eac0d1487006e82f41a6b1c7a98e737
unknown <roman.berezovskyi@nure.ua>
2022-11-07 18:50:14 +0200

    navigation-bar

---
+++ src/routes/navigation-bar/navigation-bar.component.jsx
@@ -0,0 +1,26 @@
+import { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+
+import "./navigation-bar.styles.scss";
+
+const NavigationBar = () => (
+  <Fragment>
+    <div className="navigation-bar">
+      <Link className="logo-container" to="/">
+        <div>
+          <Logo className="logo" />
+        </div>
+      </Link>
+      <div className="nav-links-container">
+        <Link className="nav-link" to="shop">
+          SHOP
+        </Link>
+      </div>
+    </div>
+    <Outlet />
+  </Fragment>
+);
+
+export default NavigationBar;
