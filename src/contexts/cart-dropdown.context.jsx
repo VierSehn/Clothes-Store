b51e095fffb413855777640e6dd367de3185cafd
VierSehn <roman.berezovskyi@nure.ua>
2023-02-11 18:52:07 +0200

    carttcomponent, cart items, adding items into cart

--- src/contexts/cart-dropdown.context.jsx
+++ src/contexts/cart-dropdown.context.jsx
@@ -1,13 +1,34 @@
 import { createContext, useState } from "react";
 
+const addCartItem = (cartItems, productToAdd) => {
+  const existingProduct = cartItems.find((item) => item.id === productToAdd.id);
+  if (existingProduct) {
+    return cartItems.map((item) =>
+      item.id === productToAdd.id
+        ? { ...item, quantity: item.quantity + 1 }
+        : item
+    );
+  } else {
+    return [...cartItems, { ...productToAdd, quantity: 1 }];
+  }
+};
+
 export const CartDropdownContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
 });
 
 export const CartDropdownProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = { isCartOpen, setIsCartOpen };
+  const [cartItems, setCartItems] = useState([]);
+
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
+
+  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
   return (
     <CartDropdownContext.Provider value={value}>
       {children}
