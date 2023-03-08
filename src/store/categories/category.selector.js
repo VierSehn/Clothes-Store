837b940c6ce499fef71d71ada43aa535ca088bdd
VierSehn <roman.berezovskyi@nure.ua>
2023-03-08 22:23:01 +0200

    reselect 2.0

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,17 @@
-export const selectCategoriesMap = (state) => 
-  state.categories.categories.reduce((accumulator, category) => {
+import { createSelector } from "reselect"
+
+const selectCategoryItem = (state) => state.categories
+
+export const selectCategories = createSelector (
+  [selectCategoryItem],
+  (categoriesSlice) => categoriesSlice.categories
+)
+
+export const selectCategoriesMap = createSelector (
+  [selectCategories],
+  (categories) => categories.reduce((accumulator, category) => {
     const { title, items } = category
     accumulator[title.toLowerCase()] = items
     return accumulator
-}, {})
+  }, {})
+)
